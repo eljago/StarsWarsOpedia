5c9ef4803865093baee09c34be178f74b821deb4
Arturo Espinoza Carrasco <clonjago@gmail.com>
2020-05-28 16:39:15 -0400

    Added array of Displayable to MainTableViewController. Setting it in the fetchFilms method. Adjusted tableview data source methods to display the data

--- StarWarsOpedia/View Controller/MainTableViewController.swift
+++ StarWarsOpedia/View Controller/MainTableViewController.swift
@@ -32,6 +32,8 @@ import Alamofire
 class MainTableViewController: UITableViewController {
   @IBOutlet weak var searchBar: UISearchBar!
   
+  var items: [Displayable] = []
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     searchBar.delegate = self
@@ -40,11 +42,14 @@ class MainTableViewController: UITableViewController {
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return items.count
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "dataCell", for: indexPath)
+    let item = items[indexPath.row]
+    cell.textLabel?.text = item.titleLabelText
+    cell.detailTextLabel?.text = item.subtitleLabelText
     return cell
   }
   
@@ -75,7 +80,9 @@ extension MainTableViewController {
     .validate()
     .responseDecodable(of: Films.self) { (response) in
       guard let films = response.value else { return }
-      print(films.all[0].title)
+      
+      self.items = films.all
+      self.tableView.reloadData()
     }
   }
 }
